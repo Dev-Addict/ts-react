b130163af796a93d6cad879461f15c2519ac147f
WolfMAN <aria.azadi.pour@protonmail.com>
2020-09-02 16:52:08 +0430

    create delete logic

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,17 +5,16 @@ import CreateTodo from "./CreateTodo";
 import Todo from "../models/Todo";
 
 const App: FC = () => {
-    const [todos, setTodos] = useState([
-        new Todo('Finish TypeScript Course.'),
-        new Todo('Finish User Experience Course.')
-    ]);
+    const [todos, setTodos] = useState<Todo[]>([]);
 
     const onCreateTodo = (text: string) => setTodos(prevState => [new Todo(text), ...prevState]);
+
+    const onDeleteTodo = (id: string) => setTodos(prevState => prevState.filter(todo => todo.id !== id));
 
     return (
         <div>
             <CreateTodo onSubmit={onCreateTodo}/>
-            <TodoList todos={todos}/>
+            <TodoList todos={todos} onDelete={onDeleteTodo}/>
         </div>
     );
 };
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,11 +3,23 @@ import React, {FC} from "react";
 import Todo from "../models/Todo";
 
 interface Props {
-    todos: Todo[]
+    todos: Todo[],
+    onDelete: (id: string) => void
 }
 
-const TodoList: FC<Props> = ({todos}) => {
-    const renderTodos = () => todos.map(({text, id}) => <li key={id}>{text}</li>);
+const TodoList: FC<Props> = ({todos, onDelete}) => {
+
+    const renderTodos = () => todos.map(({text, id}) => {
+        const onDeleteClick = (_: any) => {
+            onDelete(id);
+        };
+
+        return (
+            <li key={id}>
+                <span>{text}</span>
+                <button onClick={onDeleteClick}>DELETE</button>
+            </li>)
+    });
 
     return (
         <div>
